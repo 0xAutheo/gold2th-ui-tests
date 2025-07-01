1efaba9e78f1abd6a2be7e68ea77ca3bd924d8c6
aruna-5data <aruna.a@5datainc.com>
2025-07-01 13:46:50 +0530

    new did creation code updated SPE-240

--- tests/didMetaMaskTests.spec.ts
+++ tests/didMetaMaskTests.spec.ts
@@ -1,11 +1,21 @@
-import {test} from "../fixtures/didFixture";
+import {test,expect} from "../fixtures/didFixture";
 
 
 test.describe("Dapp Wallet Connection Test", () => {
-    test("Connect wallet and verify auth flow", async ({homePage}) => {
+    test("Connect wallet and verify auth flow", async ({homePage,page}) => {
         await homePage.navigate();
         await homePage.connectWallet();
         // Add your assertions here if needed
-        await homePage.page.waitForTimeout(3000); // placeholder for visual/manual check or continue flow
+
+        // placeholder for visual/manual check or continue flow
+
+        await page.locator("//h3[text()='DID Manager']").click()
+        await page.locator("//button[text()='Create Your First DID']").click()
+        await page.locator("//button[text()='View Credentials']").click()
+
+
+        await expect(page.locator("//div[@class='glass-card p-4 cursor-pointer hover:bg-white/5 transition-colors']")).toHaveCount(4)
+
+        await homePage.page.waitForTimeout(3000);
     });
 });
